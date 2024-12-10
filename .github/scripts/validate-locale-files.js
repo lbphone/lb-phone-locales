d628c6ce85a2f7f9265b345ebc2f3fa16c041a33
Maximus7474 <the.maximus.7474@gmail.com>
2024-12-10 21:53:37 +0100

    rf(validate-locale-files): check key naming in detail & list missing keys

--- .github/scripts/validate-locale-files.js
+++ .github/scripts/validate-locale-files.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-function validateJsonFile(filePath) {
+const enJson = JSON.parse(fs.readFileSync('en.json', 'utf-8'));
+
+function validateEnJsonFile(filePath) {
     try {
         const fileContent = fs.readFileSync(filePath, 'utf8');
         const json = JSON.parse(fileContent);
@@ -20,6 +22,45 @@ function validateJsonFile(filePath) {
         };
         checkKeys(json);
 
+        console.log(`\x1b[32mBase Locale: en.json passed all checks.\x1b[0m`);
+    } catch (error) {
+        console.error(error.message);
+        process.exitCode = 1;
+    }
+}
+
+function validateJsonFile(filePath) {
+    try {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const json = JSON.parse(fileContent);
+
+        const missingKeys = [];
+
+        const checkKeys = (obj1, obj2, parentKey = '') => {
+            const keys1 = Object.keys(obj1);
+            const keys2 = Object.keys(obj2);
+
+            for (const key of keys1) {
+                if (!keys2.includes(key)) {
+                    missingKeys.push(`${parentKey}${key}`);
+                }
+
+                if (typeof obj1[key] === 'object' && obj1[key] !== null && typeof obj2[key] === 'object' && obj2[key] !== null) {
+                    checkKeys(obj1[key], obj2[key], `${parentKey}${key}.`);
+                }
+            }
+        };
+
+        checkKeys(enJson, json);
+
+        if (missingKeys.length > 0) {
+            console.error(`\x1b[31mThe following keys are missing in the second file '${filePath}':\x1b[0m`);
+            missingKeys.forEach((key) => {
+                console.error(`- ${key}`);
+            });
+            throw new Error(`Key mismatch detected.`);
+        }
+
         console.log(`\x1b[32mFile '${filePath}' passed all checks.\x1b[0m`);
     } catch (error) {
         console.error(error.message);
@@ -28,10 +69,9 @@ function validateJsonFile(filePath) {
 }
 
 let files = process.argv.slice(2);
+const parentDir = __dirname.includes('scripts') ? path.resolve(__dirname, '../../') : __dirname;
 
 if (files.length === 0) {
-    const parentDir = path.resolve(__dirname, '../../');
-
    
     fs.readdir(parentDir, (err, allFiles) => {
         if (err) {
@@ -43,14 +83,28 @@ if (files.length === 0) {
 
         jsonFiles.forEach((file) => {
             console.log(`\x1b[34mChecking ${file}\x1b[0m`);
-            validateJsonFile(path.resolve(parentDir, file));
+            switch (file) {
+                case 'en.json':
+                    validateEnJsonFile(path.resolve(parentDir, file));
+                    break
+                default:
+                    validateJsonFile(path.resolve(parentDir, file));
+                    break;
+            }
         });
     });
 } else {
     files.forEach((file) => {
         if (file.endsWith('.json')) {
             console.log(`\x1b[34mChecking ${file}\x1b[0m`);
-            validateJsonFile(path.resolve(file));
+            switch (file) {
+                case 'en.json':
+                    validateEnJsonFile(path.resolve(parentDir, file));
+                    break
+                default:
+                    validateJsonFile(path.resolve(parentDir, file));
+                    break;
+            }
         }
     });
 }
